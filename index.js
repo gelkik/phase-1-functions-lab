33717229d1fcdc9a8acae179957484380f6552d0
George Elkik <gelkik@gmail.com>
2023-01-09 13:29:09 -0500

    commit tree 03245677062f374fc13b04d61cb4225f4e9a7944

--- index.js
+++ index.js
@@ -1,1 +1,49 @@
-// Code your solution in this file!
+
+function distanceFromHqInBlocks(block){
+    if (block >= 42){
+        return block - 42;
+    }
+
+    else if (block < 42){
+        return 42 - block;
+    }
+}
+
+function distanceFromHqInFeet(feet){
+    if (feet >= 42){
+        return (feet - 42)*264;
+    }
+
+    else if (feet < 42){
+        return (42 - feet)*264;
+    }
+}
+
+function distanceTravelledInFeet(start,destination){
+    if (destination>=start){
+        return (destination - start)*264;
+    }
+    else if (destination<start){
+        return (start - destination)*264;
+    }
+}
+
+function calculatesFarePrice(start, destination){
+    const distance = distanceTravelledInFeet(start,destination);
+
+    if (distance <= 400 ){
+        return 0;
+    }
+
+    else if (distance < 2000){
+        return (distance-400)*0.02;
+    }
+
+    else if (distance > 2000 && distance < 2500){
+        return 25;
+    }
+
+    else if (distance >= 2500){
+        return "cannot travel that far";
+    }
+}
